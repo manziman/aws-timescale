681bb4a0a67efbb7c6c749e8c49c5b6511db4ffb
Chris Manzi <chris@autosearch.io>
2019-02-17 20:48:54 -0600

    working

--- webhook/consumer.py
+++ webhook/consumer.py
@@ -1,5 +1,4 @@
-from json import loads
-from logging import getLogger
+from logging import getLevelName, getLogger, INFO
 from os import getenv
 
 from psycopg2 import connect
@@ -7,11 +6,12 @@ from psycopg2 import connect
 
 DB_PASS = getenv("POSTGRES_PASS", default="")
 DB_HOSTNAME = getenv("DB_PRIVATE_HOSTNAME", default="")
-DB_CONNECTION = connect(host=DB_HOSTNAME, database="webhooks", user="postgres", password=DB_PASS)
-LOGGER = getLogger(name="webhooks")
+DB_CONNECTION = connect(host=DB_HOSTNAME, database="webhook", user="postgres", password=DB_PASS)
 
+LOGGER = getLogger(name="webhooks")
+LOGGER.setLevel(getLevelName("INFO"))
 
 def consume(event, context):
-    body = loads(event["body"])
+    body = event["body"]
     LOGGER.info("request body: {}".format(body))
-    return
+    return body
