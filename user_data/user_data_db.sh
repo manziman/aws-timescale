3d59953f036d2922343427af3e5906b5f257d3a0
Chris Manzi <chris@autosearch.io>
2019-03-02 13:29:12 -0600

    fixed user data

--- user_data/user_data_db.sh
+++ user_data/user_data_db.sh
@@ -9,11 +9,11 @@ apt-get update -yq && apt-get upgrade -yq
 printf "12\n5\n" | apt-get install -y timescaledb-postgresql-11
 timescaledb-tune --quiet --yes
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '10.0.2.55'/g" /etc/postgresql/11/main/postgresql.conf
-sed -i "s/# IPv4 local connections:/host *all *all *10.0.2.0/24 *md5/g" /etc/postgresql/11/main/pg_hba.conf
+sed -i "s/# IPv4 local connections:/host    all             all             10.0.2.0\/24             md5/g" /etc/postgresql/11/main/pg_hba.conf
 service postgresql restart
-su - postgres -c "psql -U postgres -d postgres -c \"alter user postgres with password 'p@ssw0rd'\;\""
-su - postgres -c "PGPASSWORD='p@ssw0rd' psql -U postgres -c \"CREATE DATABASE webhook\;\""
-su - postgres -c "PGPASSWORD='p@ssw0rd' psql -U postgres -c \"CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE\;\" webhook"
-su - postgres -c "PGPASSWORD='p@ssw0rd' psql -U postgres -c \"CREATE TABLE example_data \(first_data INT not null, second_data INT not null, time TIMESTAMP not null\)\;\" webhook"
-su - postgres -c "PGPASSWORD='p@ssw0rd' psql -U postgres -c \"SELECT create_hypertable \('example_data', 'time', chunk_time_interval => interval '1 minute'\)\;\" webhook"
+su - postgres -c "psql -U postgres -d postgres -c \"alter user postgres with password 'p@ssw0rd';\""
+su - postgres -c "PGPASSWORD='p@ssw0rd' psql -U postgres -c \"CREATE DATABASE webhook;\""
+su - postgres -c "PGPASSWORD='p@ssw0rd' psql -U postgres -c \"CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;\" webhook"
+su - postgres -c "PGPASSWORD='p@ssw0rd' psql -U postgres -c \"CREATE TABLE example_data (first_data INT not null, second_data INT not null, time TIMESTAMP not null);\" webhook"
+su - postgres -c "PGPASSWORD='p@ssw0rd' psql -U postgres -c \"SELECT create_hypertable ('example_data', 'time', chunk_time_interval => interval '1 minute');\" webhook"
 service ssh start
